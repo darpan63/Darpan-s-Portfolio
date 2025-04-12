063e1f3605224479f582021789ae26b48963f4bc
darpan63 <darpansakhala11@gmail.com>
2025-04-12 17:35:56 -0400

    Adding images

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -9,7 +9,7 @@ const Projects = () => {
       description:
         "Crafted an AI-powered chatbot leveraging Python, TensorFlow and OpenAI GPT-3 alongside spaCy for NLP, with a Flask backendand React.js interface, increasing engagement by 40% and learning retention by 30%.",
       image:
-        "https://images.unsplash.com/photo-1511379938547-c1f69419868d?auto=format&fit=crop&w=800&q=80",
+        "https://dribbble.com/shots/21053716-AI-Chatbot-Mobile-App-UI",
       skills: ["|Python", "TensorFlow", "OpenAI", "NLP", "Flask","React.js"],
       liveUrl: "https://github.com/darpan63",
       githubUrl: "https://github.com/darpan63",
@@ -20,7 +20,7 @@ const Projects = () => {
       description:
         "A music-centric social platform with real-time track sharing that improved engagement by 40% and enhanced performance by 30% through optimized React components and backend APIs.",
       image:
-        "https://images.unsplash.com/photo-1511379938547-c1f69419868d?auto=format&fit=crop&w=800&q=80",
+        "https://mir-s3-cdn-cf.behance.net/project_modules/1400/3e9f7c198733857.659f6b2c4c7d1.jpg",
       skills: ["MongoDB", "Express.js", "React.js", "Node.js", "WebSockets"],
       liveUrl: "https://github.com/darpan63",
       githubUrl: "https://github.com/darpan63",
@@ -31,7 +31,7 @@ const Projects = () => {
       description:
         "Implemented a CNN-driven method for epileptic seizure identification achieving over 90% accuracy, enhanced by LSTM layers and thorough data visualization for critical clinical insights.",
       image:
-        "https://images.unsplash.com/photo-1558494949-ef010cbdcc31?auto=format&fit=crop&w=800&q=80",
+        "https://www.frontiersin.org/files/Articles/578126/fnins-14-578126-HTML/image_m/fnins-14-578126-g001.jpg",
       skills: ["Python", "CNN", "TensorFlow", "Keras", "LSTM", "Matplotlib"],
       liveUrl: "https://github.com/darpan63",
       githubUrl: "https://github.com/darpan63",
@@ -53,7 +53,7 @@ const Projects = () => {
       description:
         "A responsive flight booking platform that streamlines user interactions with Ant Design components and Tailwind-powered UI, improving booking efficiency by 35%. Built with a robust TypeScript backend using Node.js and Express for seamless data handling and API performance.",
       image:
-        "https://images.unsplash.com/photo-1548569909-466b70d5e7f5?auto=format&fit=crop&w=800&q=80",
+        "https://cdn.dribbble.com/userupload/34958259/file/original-cd389f319f8906df8f3190df4b76b13f.png?resize=752x&vertical=center",
       skills: ["React", "Tailwind CSS", "Ant Design", "Node.js", "Express", "TypeScript"],
       liveUrl: "https://github.com/darpan63",
       githubUrl: "https://github.com/darpan63",
