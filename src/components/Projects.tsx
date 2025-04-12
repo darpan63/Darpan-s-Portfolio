63fd44efe625dc61a95ccddd6b084dac4a20fe5c
darpan63 <darpansakhala11@gmail.com>
2025-04-12 17:25:30 -0400

    Some little changes

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -5,48 +5,60 @@ const Projects = () => {
   const projects = [
     {
       id: 1,
+      title: "ChronoTalk: AI History Chatbot",
+      description:
+        "Crafted an AI-powered chatbot leveraging Python, TensorFlow and OpenAI GPT-3 alongside spaCy for NLP, with a Flask backendand React.js interface, increasing engagement by 40% and learning retention by 30%.",
+      image:
+        "https://images.unsplash.com/photo-1511379938547-c1f69419868d?auto=format&fit=crop&w=800&q=80",
+      skills: ["|Python", "TensorFlow", "OpenAI", "NLP", "Flask","React.js"],
+      liveUrl: "https://github.com/darpan63",
+      githubUrl: "https://github.com/darpan63",
+    },
+    {
+      id: 2,
       title: "Harmonify",
       description:
         "A music-centric social platform with real-time track sharing that improved engagement by 40% and enhanced performance by 30% through optimized React components and backend APIs.",
       image:
         "https://images.unsplash.com/photo-1511379938547-c1f69419868d?auto=format&fit=crop&w=800&q=80",
       skills: ["MongoDB", "Express.js", "React.js", "Node.js", "WebSockets"],
-      liveUrl: "#",
-      githubUrl: "#",
+      liveUrl: "https://github.com/darpan63",
+      githubUrl: "https://github.com/darpan63",
     },
     {
-      id: 2,
+      id: 3,
       title: "Epileptic Seizure Detection",
       description:
         "Implemented a CNN-driven method for epileptic seizure identification achieving over 90% accuracy, enhanced by LSTM layers and thorough data visualization for critical clinical insights.",
       image:
         "https://images.unsplash.com/photo-1558494949-ef010cbdcc31?auto=format&fit=crop&w=800&q=80",
       skills: ["Python", "CNN", "TensorFlow", "Keras", "LSTM", "Matplotlib"],
-      liveUrl: "#",
-      githubUrl: "#",
+      liveUrl: "https://github.com/darpan63",
+      githubUrl: "https://github.com/darpan63",
     },
     {
-      id: 3,
+      id: 4,
       title: "NYC School Bus Delays Analysis",
       description:
         "Engineered an ETL pipeline for 600K+ NYC school bus delay records using Python and Airflow, storing raw data in AWS S3, transforming it in PostgreSQL, and delivering insights via Power BI dashboards.",
       image:
         "https://images.unsplash.com/photo-1580508174046-170816f65662?auto=format&fit=crop&w=800&q=80",
       skills: ["SQL", "Python", "Airflow", "AWS S3", "PostgreSQL", "Power BI"],
-      liveUrl: "#",
-      githubUrl: "#",
+      liveUrl: "https://github.com/darpan63",
+      githubUrl: "https://github.com/darpan63",
     },
     {
-      id: 4,
-      title: "E-Commerce Platform",
+      id: 5,
+      title: "SkyReserve",
       description:
-        "Deployed a fully featured e-commerce platform for the Australian market using React, Node.js, and MySQL, boosting revenue by 15% and expanding the client's market presence.",
+        "A responsive flight booking platform that streamlines user interactions with Ant Design components and Tailwind-powered UI, improving booking efficiency by 35%. Built with a robust TypeScript backend using Node.js and Express for seamless data handling and API performance.",
       image:
-        "https://images.unsplash.com/photo-1556742049-0cfed4f6a45d?auto=format&fit=crop&w=800&q=80",
-      skills: ["React", "Node.js", "MySQL", "OAuth", "RESTful APIs"],
-      liveUrl: "#",
-      githubUrl: "#",
-    },
+        "https://images.unsplash.com/photo-1548569909-466b70d5e7f5?auto=format&fit=crop&w=800&q=80",
+      skills: ["React", "Tailwind CSS", "Ant Design", "Node.js", "Express", "TypeScript"],
+      liveUrl: "https://github.com/darpan63",
+      githubUrl: "https://github.com/darpan63",
+    }
+
   ];
 
   return (
